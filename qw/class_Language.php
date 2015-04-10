7df1a4538236667a05eb70d88824dee3a8d26911
Tomáš Babický <babicto1@fel.cvut.cz>
2015-04-10 16:45:18 +0200

    Refactoring Language

--- qw/class_Language.php
+++ qw/class_Language.php
@@ -5,8 +5,13 @@
 /**
  * Class Language
  */
-class Language {
+class Language implements ILanguage {
     private $langName, $lang, $meta, $allPackages;
+
+    const PATH = './lang/';
+    const PREFIX_NAME = '/lang';
+    const PREFIX_NAME_META = '/langMeta';
+    const EXT = '.ini';
 
     /**
      * @param $langName
@@ -18,21 +23,21 @@ class Language {
 
         $this->langName = $langName;
 
+        // we dont have any other langs yet
         if ( $this->langName != 'CZ' )
             $this->langName = 'CZ';
 
-        if ( !file_exists('./lang/'.$this->langName) )
+        if ( !file_exists(self::PATH.$this->langName) )
             throw new LanguageException('Neexistující celý jazykový balíček: <strong>'.$this->langName.'</strong>.');
 
-        if ( !file_exists('./lang/'.$this->langName.'/lang'.$this->langName.'.ini') )
+        if ( !file_exists(self::PATH.$this->langName.self::PREFIX_NAME.$this->langName.self::EXT) )
             throw new LanguageException('Neexistující jazykový balíček: <strong>'.$langName.'</strong>.');
 
-        $this->lang = parse_ini_file('./lang/'.$this->langName.'/lang'.$this->langName.'.ini');
-
-        if ( !file_exists('./lang/'.$this->langName.'/langMeta'.$this->langName.'.ini') )
+        if ( !file_exists(self::PATH.$this->langName.self::PREFIX_NAME_META.$this->langName.self::EXT) )
             throw new LanguageException('Neexistující jazykový balíček meta dat: <strong>'.$langName.'</strong>.');
 
-        $this->meta = parse_ini_file('./lang/'.$this->langName.'/langMeta'.$this->langName.'.ini');
+        $this->lang = parse_ini_file(self::PATH.$this->langName.self::PREFIX_NAME.$this->langName.self::EXT);
+        $this->meta = parse_ini_file(self::PATH.$this->langName.self::PREFIX_NAME_META.$this->langName.self::EXT);
     }
 
     /**
@@ -66,7 +71,7 @@ class Language {
      *
      */
     public function languageSetAllPackages() {
-        foreach ( glob('./lang/*') as $v )
+        foreach ( glob(self::PATH.'*') as $v )
             $this->allPackages[] = $v;
     }
 
