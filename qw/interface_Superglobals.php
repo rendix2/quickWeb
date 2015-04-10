70c611a3fd2a9106740f6b267a11b1f4f6dfb6b2
Tomáš Babický <babicto1@fel.cvut.cz>
2015-04-10 18:54:46 +0200

    Interface Superglobals

--- qw/interface_Superglobals.php
+++ qw/interface_Superglobals.php
@@ -2,10 +2,7 @@
 
 interface Superglobals{
     public static function exists($name);
-
     public static function get($name);
-
     public static  function put($name, $value, $expire = null);
-
     public static  function delete($name);
 }
